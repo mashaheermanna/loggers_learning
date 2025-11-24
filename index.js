de8ee481f7d37c635a1e5fed8bb2240b7d998354
Mashaheer Manna <mashaheer03@gmail.com>
2025-11-24 17:24:43 +0530

    log to the file, combined and error

---
+++ index.js
@@ -0,0 +1,17 @@
+import logger from "./logger/logger.js";
+// const logger = require("./logger/logger");
+
+// logger.log("logger.log messge");
+logger.error("console.error message level is 0");
+logger.warn("console.warn message level is 1");
+logger.info("console.info message level is 2");
+
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  verbose: 4,
+  debug: 5,
+  silly: 6,
+};
